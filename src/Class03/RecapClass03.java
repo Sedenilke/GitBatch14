9061d330de8fd3eee5f85f4a80399ab6593e390e
Sedenilke <sedenilke@gmail.com>
2022-09-27 16:51:49 -0700

    second commit

--- src/Class03/RecapClass03.java
+++ src/Class03/RecapClass03.java
@@ -4,7 +4,7 @@ public class RecapClass03 {
 
 	public static void main(String[] args) {
 		System.out.println("hey this is a git class");
-
+		System.out.println("This is a change in my project");
 	}
 
 }
